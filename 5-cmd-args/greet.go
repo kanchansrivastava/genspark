372288348860b7552128fc669194d4d786250c84
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-11-25 09:24:39 +0530

    .

---
+++ 5-cmd-args/greet.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	greet()
+}
+
+func greet() {
+	data := os.Args[1:] // 1st index till the end
+	if len(data) != 3 {
+		log.Println("please provide name age marks")
+		//os.Exit(1) // quit the program
+		return // stops the exec of the current func
+	}
+	//var err error // default value is nil // it indicates no error happened
+	//if err != nil {
+	//	// handle the error here
+	//}
+
+	name := data[0]
+	ageString := data[1]
+	//marksString := data[2]
+
+	age, err := strconv.Atoi(ageString)
+	if err != nil {
+		log.Println("invalid age", err)
+
+	}
+	_, _ = age, name
+	//fmt.Println(name, age)
+	//strconv.ParseFloat()
+}

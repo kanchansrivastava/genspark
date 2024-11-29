893b6dc0a90e5a2bb4bdbf90c6d4e97f0efb40f5
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-11-29 21:36:23 +0530

    .

--- 10-interface/empty-interface.go
+++ 10-interface/empty-interface.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 
+	// any can store any type of value
+	// it is an empty interface with 0 methods defined
+	// any is an alias of interface{}
 	var a any // var i interface{}
 	a = 10
 	a = struct{ a int }{}
@@ -11,7 +14,7 @@ func main() {
 	var b bool
 	a = "hello"
 	_, _ = a, b
-	//b, ok := a.(bool)
+	//b, ok := a.(bool) // type assertion to fetch concrete value from any type
 	//if !ok {
 	//	fmt.Println("not of bool type")
 	//	return
--- 10-interface/log.go
+++ 10-interface/log.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 )
 
 type user struct {
 	name  string
 	email string
+}
+
+func (u user) Write(p []byte) (n int, err error) {
+	fmt.Printf("sending notification to %s %s\n", u.name, u.email)
+	return len(p), nil
 }
 
 func main() {

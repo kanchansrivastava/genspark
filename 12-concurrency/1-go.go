cf277f9f6618f01061148e789d756a4122d777e4
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-12-04 14:20:24 +0530

    .

--- 12-concurrency/1-go.go
+++ 12-concurrency/1-go.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+// concurrency is dealing with a lot of things at once
+// parallelism is doing multiple things at once
+
+// Concurrency is not Parallelism by Rob Pike
+// https://www.youtube.com/watch?v=oV9rvDllKEg&ab_channel=gnbitcom
 func main() {
 	//panic("panic") // reveals goroutine id and name
 	go hello()

b7ce740381f5d657ed563e237b381117d0ac50b5
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-12-05 12:31:35 +0530

    .

--- 12-concurrency/11-range.go
+++ 12-concurrency/11-range.go
@@ -7,13 +7,16 @@ import (
 
 func main() {
 	wg := new(sync.WaitGroup)
+	wgWorker := new(sync.WaitGroup)
 	ch := make(chan int)
-	wg.Add(2)
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		for i := 1; i <= 5; i++ {
+			wgWorker.Add(1)
 			// fan out pattern, spinning up n number of goroutines, for n number of task
 			go func() {
+				defer wgWorker.Done()
 				ch <- i
 			}()
 
@@ -23,15 +26,26 @@ func main() {
 
 		// we can't send more values after a channel is closed
 		//ch <- 6 // panic: send on closed channel // channel is closed
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		wgWorker.Wait() // until workers are not finished, we would wait
+		// close the channel if workers are done sending
 		close(ch)
 	}()
 
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
+		// range gives a guarantee everything would be received
 		for v := range ch {
 			fmt.Println(v)
 		}
 	}()
 
 	wg.Wait()
+	fmt.Println("Done")
+
 }

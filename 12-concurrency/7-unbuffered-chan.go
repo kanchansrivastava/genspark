34ef4fd8c2757ee3274a893c7cc6686556c58835
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-12-05 10:09:20 +0530

    .

--- 12-concurrency/7-unbuffered-chan.go
+++ 12-concurrency/7-unbuffered-chan.go
@@ -8,19 +8,26 @@ import (
 
 func main() {
 	wg := new(sync.WaitGroup)
-	ch := make(chan int)
+	ch := make(chan int) // unbuf chan
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
 
 		fmt.Println("sender picked up")
 		ch <- 1
+		// in our case sender would be picked up first , because recv is blocked by a sleep
+		// but when go sched try to run this routine, this would also block,
+		// because there is no recv to send the values
+		// so this goroutine goes in the blocked state until recv is up
+		// once recv routine runs this routine would be unblocked and it will send the value and move on
 	}()
 	go func() {
 		defer wg.Done()
 		time.Sleep(time.Second)
 		fmt.Println("recv picked up")
 		fmt.Println(<-ch)
+		// recv would be read after a second
+		//and it would wait for sender to send the values
 	}()
 	wg.Wait()
 }

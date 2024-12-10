f29599eb43738dba99c57c87f2d593c247392541
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-12-10 11:28:28 +0530

    .

--- 13-context/2-ctx.go
+++ 13-context/2-ctx.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	i, err := Slow(ctx, "ac")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println(i)
+}
+func Slow(ctx context.Context, input string) (int, error) {
+	//sql.DB{}.ExecContext(ctx, "SELECT 1")
+	i, err := strconv.Atoi(input)
+	if err != nil {
+		return 0, fmt.Errorf("error converting %s to int: %w", input, err)
+	}
+	time.Sleep(2 * time.Millisecond)
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	default:
+		return i, nil
+	}
+}

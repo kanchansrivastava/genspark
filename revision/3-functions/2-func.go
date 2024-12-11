1c6fbafa52ca0eef6cb2282dc96362ce4dc715ff
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-12-11 14:27:22 +0530

    .

--- revision/3-functions/2-func.go
+++ revision/3-functions/2-func.go
@@ -1,19 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type operation func(int, int) int
+type money int
+
+type person struct{}
+type i = interface{} // alias
 
 func main() {
+
 	operate(add, 10, 20)
+	operateV2(add, 10, 23)
 	//add(10, 20)
 }
 
 // operate func can accept function in op parameter,
 // the function signature we are passing should match to op parameter type
-func operate(op func(int, int), x, y int) {
-	op(x, y)
+func operate(op func(int, int) int, x, y int) {
+	fmt.Println(op(x, y))
+}
+
+// operate func can accept function in op parameter,
+// the function signature we are passing should match to op parameter type
+func operateV2(op operation, x, y int) {
+	fmt.Println(op(x, y))
 }
 
 func print(i int, s string) {}
 func add(a, b int) int {
-	fmt.Println(a + b)
+	return a + b
 }

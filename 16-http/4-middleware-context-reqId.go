3d3e6951b02fa0cfd7db346468b1db911a5e6649
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-12-13 14:15:01 +0530

    .

--- 16-http/4-middleware-context-reqId.go
+++ 16-http/4-middleware-context-reqId.go
@@ -29,22 +29,22 @@ func ReqIdMid(next http.HandlerFunc) http.HandlerFunc {
 		ctx := r.Context()                         // fetching the ctx object from the request
 		ctx = context.WithValue(ctx, ReqIdKey, id) // creating an updated ctx with a traceId store in it
 		r = r.WithContext(ctx)                     // putting context inside the request object
-		next(w, r)
+		next(w, r)                                 // calling next thing in the chain
 
 	}
 }
 func LogMid(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		t := time.Now()
-		ctx := r.Context()
-		val := ctx.Value(ReqIdKey)
-		reqId, ok := val.(ContextKey)
+		ctx := r.Context()            // fetching the context from the request
+		val := ctx.Value(ReqIdKey)    // fetch the request id for the key
+		reqId, ok := val.(ContextKey) // checking if the values exist and of correct type
 		if !ok {
 			reqId = "unknown"
 		}
 		log.Printf("reqId, method, url: %s, %s, %s\n", reqId, r.Method, r.URL)
 		defer log.Printf("reqId,  duration: %s, %s\n", reqId, time.Since(t))
-		next(w, r)
+		next(w, r) // calling next thing in the chain
 
 	}
 

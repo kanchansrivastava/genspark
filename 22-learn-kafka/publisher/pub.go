378c7583ca3f632d7ff524d3d682f9d296a19a79
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-12-23 11:39:16 +0530

    .

--- 22-learn-kafka/publisher/pub.go
+++ 22-learn-kafka/publisher/pub.go
@@ -1,1 +1,9 @@
-package publisher
+package main
+
+// connect kafka using kgo
+
+// create a record using kgo.Record{key:value}
+
+// produceRecord:=client.ProduceSync(ctx,record)
+
+// produceRecord.First()

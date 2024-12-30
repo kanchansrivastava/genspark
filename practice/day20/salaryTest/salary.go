b10ee1c48f2c1d5695054a87aa66f8bd63ca48a6
Kanchan Srivastava <kanchansrivastava1991@gmail.com>
2024-12-30 20:24:09 +0530

    Add file (may be deleted later)

---
+++ practice/day20/salaryTest/salary.go
@@ -0,0 +1,28 @@
+/*
+Q1. Create a struct user,
+    Fields: Name, Email, Pay, Bonus
+
+    Create a method that calculates total salary for a month for a user
+    Write test for it
+
+
+*/
+
+package main
+
+type User struct {
+	name string
+	email string
+	pay int
+	bonus int
+}
+
+
+func calculateMonthlySalary() {
+
+}
+
+
+func main() {
+
+}
---
+++ practice/day8/question1.go
@@ -0,0 +1,5 @@
+// q4. Create a map in postgres.go and mysql.go to store user details in the memory
+//     Change the signature of methods (Create(user) bool, Update(name) bool, Delete(id) bool)
+//     Note:- true would indicate operation has been successful.
+//     Add two new methods FetchAll, FetchUser(id)
+//     This time write concrete functionalities not simple print statements
